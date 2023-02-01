12c05cc50ed90e05bba1b9c36c2c57767183d135
Ernesto Herrera <ernerdo@gmail.com>
2023-01-31 21:29:44 -0600

    add axios and implement endpoint for MovieApiClient

--- src/pages/MovieDetail/MovieDetail.tsx
+++ src/pages/MovieDetail/MovieDetail.tsx
@@ -1,7 +1,11 @@
 import { GridItem } from '@chakra-ui/react'
+import { getMovieDetails } from '../../client/MovieApiClient'
 import DefaultLayout from '../../layout/DefaultLayout/DefaultLayout'
 
 const MovieDetail = () => {
+  getMovieDetails(123).then((res) => {
+    console.info(res)
+  })
   return (
     <DefaultLayout>
       <GridItem pl="2" bg={'gray.300'} area={'main'}>
