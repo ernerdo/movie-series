e7d612f385f0d93bd0ef3f61594d2be2374b0639
Ernesto Herrera <ernerdo@gmail.com>
2023-03-12 14:30:25 -0600

    add to folder cast and movies the models

--- src/layout/CarouselMovies/CarouselMovies.tsx
+++ src/layout/CarouselMovies/CarouselMovies.tsx
@@ -1,6 +1,6 @@
 import { Box, Flex } from '@chakra-ui/react'
 import { FC } from 'react'
-import { CarouselSwiper } from '../../components'
+import { Carousel } from '../../components'
 import { PopularMovie } from '../../models/movies/popular.model'
 
 interface CarouselProps {
@@ -14,7 +14,7 @@ const CarouselMovies: FC<CarouselProps> = ({ title, popularMovies }) => {
       <Box as="h2" mb="16px" color="white" fontSize="1.4rem">
         {title}
       </Box>
-      <CarouselSwiper popularMovies={popularMovies}></CarouselSwiper>
+      <Carousel popularMovies={popularMovies}></Carousel>
     </Flex>
   )
 }
