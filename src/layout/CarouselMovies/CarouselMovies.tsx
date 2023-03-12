d66343e8914770fcd84dafb20ddfca0ac31eb911
Ernesto Herrera <ernerdo@gmail.com>
2023-03-11 22:21:42 -0600

    change movies by popularMovies and remove genres unused variable

--- src/layout/CarouselMovies/CarouselMovies.tsx
+++ src/layout/CarouselMovies/CarouselMovies.tsx
@@ -1,22 +1,20 @@
 import { Box, Flex } from '@chakra-ui/react'
 import { FC } from 'react'
 import { CarouselSwiper } from '../../components'
-import { Genres } from '../../models/categories/categories.model'
 import { PopularMovie } from '../../models/movies/popular.model'
 
 interface CarouselProps {
   title?: string
-  genres?: Genres
-  movies: PopularMovie[]
+  popularMovies: PopularMovie[]
 }
 
-const CarouselMovies: FC<CarouselProps> = ({ title, movies }) => {
+const CarouselMovies: FC<CarouselProps> = ({ title, popularMovies }) => {
   return (
     <Flex direction="column">
       <Box as="h2" mb="16px" color="white" fontSize="1.4rem">
         {title}
       </Box>
-      <CarouselSwiper popularMovies={movies}></CarouselSwiper>
+      <CarouselSwiper popularMovies={popularMovies}></CarouselSwiper>
     </Flex>
   )
 }
