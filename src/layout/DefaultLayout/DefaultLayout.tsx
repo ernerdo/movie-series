c994c51c35d016348d34022a009e0b022b3179e6
Ernesto Herrera <ernerdo@gmail.com>
2023-03-26 15:04:32 -0600

    hide header in detail page

--- src/layout/DefaultLayout/DefaultLayout.tsx
+++ src/layout/DefaultLayout/DefaultLayout.tsx
@@ -2,23 +2,28 @@ import React from 'react'
 import { Grid } from '@chakra-ui/react'
 import { Header } from '../Header'
 import { Footer } from '../Footer'
+import { useLocation } from 'react-router-dom'
 
 type Props = {
   children?: React.ReactNode
 }
 const DefaultLayout = ({ children }: Props) => {
+  const location = useLocation()
+  const detailRouteRegex = /^\/detail\/\d+$/
+  const isDetailRoute = detailRouteRegex.test(location.pathname)
+  const headerSize = !isDetailRoute ? '120px' : '0px'
   return (
     <Grid
       templateAreas={`"header"
                     "main"
                     "footer"`}
-      gridTemplateRows={'120px 1fr 100px'}
+      gridTemplateRows={`${headerSize} 1fr 100px`}
       gridTemplateColumns={'100%'}
       gap="1"
       bg="#0E1219"
       fontWeight="bold"
     >
-      <Header />
+      {!isDetailRoute && <Header />}
       {children}
       <Footer />
     </Grid>
