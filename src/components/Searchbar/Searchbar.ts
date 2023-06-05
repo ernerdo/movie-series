dc24da458824c7a7bd74dda86d75f92ee2a963e4
darkusphantom <luisfrodriguezb.lf@gmail.com>
2023-06-05 12:34:31 -0400

    wip: searchbar

---
+++ src/components/Searchbar/Searchbar.ts
@@ -0,0 +1,15 @@
+import { Input } from 'chakra-ui'
+
+const Searchbar = () => {
+  return (
+    <Input
+      w="70%"
+      maxW=""
+      h="30px"
+      color="white"
+      placeholder="Search a movie, serie, stream..."
+    />
+  )
+}
+
+export { Searchbar }
